62a8a9cc459ccc1b10e2dc836a0c466f8a659c26
agrawalrohant <agrawalrohant@gmail.com>
2020-05-17 18:39:31 +0530

    creating a generic component

---
+++ au-input/src/app/lib/common/input-ref.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: 'au-fa-input input'
+})
+export class InputRefDirective {
+
+  focus: boolean = false;
+
+  constructor() { }
+
+
+  @HostListener('focus')
+  onFocus() {
+    this.focus = true;
+  }
+
+  @HostListener('blur')
+  onBlur() {
+    this.focus = false;
+  }
+
+}
